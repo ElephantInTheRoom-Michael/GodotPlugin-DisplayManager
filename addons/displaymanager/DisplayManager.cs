4a7864edea753dd5bf890eae1a8d043d44a8356f
Michael Taylor <241386356+ElephantInTheRoom-Michael@users.noreply.github.com>
2026-01-08 22:55:34 -0800

    Add input action for fullscreen toggle on plugin enable and wire it up

--- addons/displaymanager/DisplayManager.cs
+++ addons/displaymanager/DisplayManager.cs
@@ -4,4 +4,34 @@ namespace DisplayManagerPlugin.addons.displaymanager;
 
 public partial class DisplayManager : Node
 {
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        if (!@event.IsActionPressed("fullscreen_toggle")) return;
+        
+        ToggleFullscreen();
+        
+        GetViewport().SetInputAsHandled();
+    }
+
+    private static void ToggleFullscreen()
+    {
+        GD.Print("Fullscreen toggle");
+        if (IsFullscreen())
+        {
+            GD.Print("Switch to windowed display mode");
+            DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+        }
+        else
+        {
+            GD.Print("Switch to fullscreen display mode");
+            DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+        }
+    }
+    
+    private static bool IsFullscreen()
+    {
+        return DisplayServer.WindowGetMode() == DisplayServer.WindowMode.Fullscreen;
+    }
 }
